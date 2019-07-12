fe4a9733d9ade220be126b9e7e900e65ffcf36f9
League Student <league@iMac-23.attlocal.net>
2019-07-12 14:28:03 -0700

    day

---
+++ src/_02_robot_graffiti/robotgraffiti.java
@@ -0,0 +1,26 @@
+package _02_robot_graffiti;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class robotgraffiti {
+public static void main(String[] args) {
+	
+Robot rob = new Robot();
+rob.penDown();
+rob.turn(90);
+rob.move(120);
+rob.turn(120);
+rob.move(200);
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
